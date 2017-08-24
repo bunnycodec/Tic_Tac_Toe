5d1048ffb785bd12c9bb6837fdc09a8ae914904f
Sunny Kumar <kumarsunnydevilx@gmail.com>
2017-08-24 22:21:30 +0530

    Better

--- java.js
+++ java.js
@@ -2,10 +2,10 @@
 $(document).ready(function() {
     const box = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     var PLAYER = "X";
-    $("#register").hide();
+    $("#register, table, #restart").hide();
     var PLAY1="",PLAY2="",active="";
     function isGameOver() {
-        
+
         if(box[0]!==" " && box[0]===box[3] && box[3]===box[6])
             return true;
         else if(box[1]!==" " && box[1]===box[4] && box[4]===box[7])
@@ -22,23 +22,18 @@ $(document).ready(function() {
             return true;
         else if(box[2]!==" " && box[2]===box[4] && box[4]===box[6])
             return true;
-    
+
         return false;
     }
-    
+
     $('#result').click(function() {
         $("#register").toggle('slow');
     });
-    
+
     $('.col').click(function() {
-            const i = $(this).data('i');
-            box[i] = PLAYER;
-        
-        if(PLAYER === "Y")
-            $("#result").html("Your Turn ... "+PLAY1);
-        else
-            $("#result").html("Your Turn ... "+PLAY2);
-        
+        const i = $(this).data('i');
+        box[i] = PLAYER;
+
         if(isGameOver() === true && PLAYER!==" "){
             $(this).html(PLAYER);
             if(PLAYER === "X")
@@ -48,12 +43,20 @@ $(document).ready(function() {
             PLAYER=" ";
         }
         else if(PLAYER!==" "){
+            if(PLAYER === "Y")
+                $("#result").html("Your Turn ... "+PLAY1);
+            else
+                $("#result").html("Your Turn ... "+PLAY2);
             if(box.indexOf(" ")===-1){
                 $(this).html(PLAYER);
                 PLAYER=" ";
                 $("#result").html("The Game is a Tie");
             }
             else{
+                if(PLAYER === "Y")
+                    $("#result").html("Your Turn ... "+PLAY1);
+                else
+                    $("#result").html("Your Turn ... "+PLAY2);
                 $(this).html(PLAYER);
                 if(PLAYER === "X")
                     PLAYER = "Y";
@@ -62,15 +65,16 @@ $(document).ready(function() {
             }
         }
     });
-    
+
     $('#submit').click(function() {
         PLAY1= document.getElementById('play1').value;
         PLAY2= document.getElementById('play2').value;
         $("#result").html("Your Turn ... "+PLAY1);
         active = PLAY1;
         $("#register").hide(1000);
+        $("#restart, table").toggle('slow');
     });
-    
+
     $('#restart').click(function() {
         for(var i=0; i < 9; i++){
             box[i]= " ";
@@ -87,7 +91,7 @@ $(document).ready(function() {
             $("#result").html("Your Turn ... "+PLAY1);
         }
     });
-    
+
 });
 
 //    
